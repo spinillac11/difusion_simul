33d8d49e06630f981c5b191ce749cdf2173c7987
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2024-03-30 06:28:30 +0000

    Implementacion funcion para inicializar posiciones

--- main.cpp
+++ main.cpp
@@ -4,53 +4,51 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <random>
 
 struct Particle{
-    double x = 50;
-    double y = 50;
+    //Posiciones de la particula
+    double x = 0;
+    double y = 0;
+    //Particula valida dentro de la simulacion
+    bool valid = true;
 
-    //Constructor
-    //Particle(double xint, double yint){
-       //this -> x = xint;
-        //this -> y = yint;
-    //}
-
-    void move(double xmax, double ymax){
+    void move(){
         int direction = rand() % 4;
         switch(direction){
             //Abajo
             case 0:
-                if (y > 0){
+                if (y >= 0){
                     y -= 1;
                 }else{
-                    y += 1;
+                    valid = false;
                 }
                 break;
 
             //Arriba
             case 1:
-                if (y < ymax){
+                if (y <= 100){
                     y += 1;
                 }else{
-                    y -= 1;
+                    valid = false;
                 }
                 break;
 
             //Izquierda
             case 2:
-                if (x > 0){
+                if (x >= 0){
                     x -= 1;
                 }else{
-                    x += 1;
+                    valid = false;
                 }
                 break;
 
             //Derecha
             case 3:
-                if (x < xmax){
+                if (x <= 100){
                     x += 1;
                 }else{
-                    x -= 1;
+                    valid = false;
                 }
                 break;
         }
@@ -60,23 +58,38 @@ struct Particle{
 
 typedef std::vector<Particle> particles;
 
-//void init(particles & position);
+void init(particles & position, int seed);
 void update(particles & position, int nsteps);
 
 int main(){
-    int Nmol = 500;
+    int Nmol = 400;
     int LatSize = 64;
-    int Nsteps = 1000;
-    int seed = 0;
+    int Nsteps = 100000;
+    int SEED = 0;
 
     particles prueba1;
     prueba1.resize(Nmol);
 
     //Semilla aleatoria 
     srand(time(NULL));
-    update(prueba1, Nsteps);
+    init(prueba1, SEED);
+    //update(prueba1, Nsteps);
 
     return 0;
+}
+
+//Incializar posiciones
+void init(particles & position, int seed){
+    int N = position.size();
+    
+    std::mt19937 generator(seed);
+    std::uniform_real_distribution<double> distribution(0.1, 0.9);
+
+    for(int jj = 0; jj < N; jj++){
+        position[jj].x = 45 + 10*distribution(generator);
+        position[jj].y = 45 + 10*distribution(generator);
+
+    }
 }
 
 //actualizar posiciones
@@ -88,10 +101,14 @@ void update(particles & position, int nsteps){
     for(int ii = 0; ii < nsteps; ii++){
         outfile << ii << "\t";
         for(int jj = 0; jj < N; jj++){
-            position[jj].move(100, 100);
-            outfile << position[jj].x << "\t" << position[jj].y << "\t";
+            //Verificar una posicion valida
+            if(position[jj].valid){
+                position[jj].move();
+            }
+        outfile << position[jj].x << "\t" << position[jj].y << "\t";
         }
         outfile << "\n";
     }
     outfile.close();
 }
+
